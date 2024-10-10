3c6ed2e893d666683da9009be52641f35e57c893
Melika Ahmadi Ranjbar <45814344+meliiwamd@users.noreply.github.com>
2024-10-10 01:17:05 -0400

    Delete task3.sh
    Adding the updated version

--- task3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This script lists files with specific criteria, sorts them, and renames them in a single pipeline.
-gawk -F, 'NR > 1 && $3 == 2 && $NF ~ "S" {print $0}' titanic.csv > filtered.csv && mv filtered.csv titanic.csv| 
-gawk -F, '{gsub(/female/, "F"); print $0}' |
-gawk -F, '{gsub(/male/, "M"); print $0}'|
-gawk -F, '{sum += $7} END {sum /= (NR - 1)} END {print sum}' 
